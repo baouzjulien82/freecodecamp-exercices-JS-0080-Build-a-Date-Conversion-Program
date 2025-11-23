5d035461431296b7087245a149e1729e28aa1a97
Julien Baouz <baouzjulien@gmail.com>
2025-11-23 12:18:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const currentDate = new Date();
+const currentDateFormat = `Current Date and Time: ${currentDate}`;
+console.log(currentDateFormat);
+
+function formatDateMMDDYYYY(dateObj) {
+  const formated = dateObj.toLocaleDateString('en-US');
+  return `Formatted Date (MM/DD/YYYY): ${formated}`;
+};
+
+function formatDateLong(dateObj) {
+  const longDate = dateObj.toLocaleDateString('en-US', {
+  //weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+});
+  return `Formatted Date (Month Day; Year): ${longDate}`;
+};
